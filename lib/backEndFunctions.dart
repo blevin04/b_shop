1312e59d08e545554faf9c095c194a00ae684cc3
blevin04 <jamestonito04@gmail.com>
2024-10-31 23:49:17 +0300

    firestore

---
+++ lib/backEndFunctions.dart
@@ -0,0 +1,6 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+final storage = FirebaseStorage.instance.ref();
