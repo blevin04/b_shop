ee5baec8fcd24e5753d7753fddbd39ce44f15abd
blevin04 <jamestonito04@gmail.com>
2024-10-31 23:42:04 +0300

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:b_shop/firebase_options.dart';
 import 'package:b_shop/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main()async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
